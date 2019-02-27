1a05952758ac09cc3fa7d0ca30f4474c51cd83cb
psw58 <psw58@cornell.edu>
2019-02-27 12:35:14 -0500

    twaek

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 // http://eslint.org/docs/user-guide/configuring
 module.exports = {
-    'root': true,
-    'parser': 'babel-eslint',
-    'parserOptions': {
-      'sourceType': 'module',
-    },
-    'plugins': [
-      'react',
-    ],
-    'env': {
-      'browser': true,
-      'es6': true,
-    },
-    'extends': [
-      // Load default configuration for react
-      'plugin:react/recommended',
-      // If you were to extend another popular
-      // eslint config, you'd put it here
-    ],
-    // add your custom rules here
-    'rules': {
-      // allow debugger during development
-      'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-      // Default no-unused-vars
-      'no-unused-vars': 1,
-      // jsx version of no-unused-vars
-      'react/jsx-uses-vars': 1,
-    }
+  'root': true,
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    // Load default configuration for react
+    'plugin:react/recommended',
+    // If you were to extend another popular
+    // eslint config, you'd put it here
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // Default no-unused-vars
+    'no-unused-vars': 1,
+    // jsx version of no-unused-vars
+    'react/jsx-uses-vars': 1,
   }
+}
